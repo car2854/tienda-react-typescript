1f19eeea12ee8aae1f877085958013628681d6a9
car2854 <car2854@gmail.com>
2024-08-01 20:12:34 -0400

    button style

--- src/pages/cart/CartPage.tsx
+++ src/pages/cart/CartPage.tsx
@@ -45,64 +45,62 @@ export const CartPage = () => {
       
         <div className="cart">
 
-
-            
-              <div className="detail-cart">
-                {cart.map((value, index: number) => (
-                  <ul key={index}>
-                    <li>
-                      <button className="remove" onClick={() => {
-                        dispatch(removeProduct(value.product))
-                      }}>
-                        <i className="fa-solid fa-x"></i>
-                      </button>
-                      <div className="product">
-                        <div className="image">
-                          <ImageComponent srcImage={value.product.image} altImage={value.product.title}></ImageComponent>
-                        </div>
-                        <div className="description">
-                          <h3>{value.product.title}</h3>
-                          <p className="product-description">{value.product.description}</p>
-                          <p className="quantity">
-                            <strong>Quantity:</strong>
-                            <button onClick={ () => {
-                              handleUpdateQuantityProduct({
-                                product: value.product,
-                                quantity: value.quantity - 1
-                              })}
-                            }><i className="fa-solid fa-chevron-left"></i></button>
-                            {value.quantity}
-                            <button onClick={() => {
-                              handleUpdateQuantityProduct({
-                                product: value.product,
-                                quantity: value.quantity + 1
-                              });
-                            }}><i className="fa-solid fa-chevron-right"></i></button>
-                          </p>
-                          <div className="price">
-                            <p><strong>Price per item:</strong> ${value.product.price}</p>
-                            <p><strong>Total price:</strong> ${money.mul(value.quantity.toFixed(2).toString(), value.product.price.toFixed(2).toString())}</p>
-                          </div>
-                        </div>
+          <div className="detail-cart">
+            <ul>
+              {cart.map((value, index: number) => (
+                <li key={index}>
+                  <button className="remove" onClick={() => {
+                    dispatch(removeProduct(value.product))
+                  }}>
+                    <i className="fa-solid fa-x"></i>
+                  </button>
+                  <div className="product">
+                    <div className="image">
+                      <ImageComponent srcImage={value.product.image} altImage={value.product.title}></ImageComponent>
+                    </div>
+                    <div className="description">
+                      <h3>{value.product.title}</h3>
+                      <p className="product-description">{value.product.description}</p>
+                      <p className="quantity">
+                        <strong>Quantity:</strong>
+                        <button onClick={ () => {
+                          handleUpdateQuantityProduct({
+                            product: value.product,
+                            quantity: value.quantity - 1
+                          })}
+                        }><i className="fa-solid fa-chevron-left"></i></button>
+                        {value.quantity}
+                        <button onClick={() => {
+                          handleUpdateQuantityProduct({
+                            product: value.product,
+                            quantity: value.quantity + 1
+                          });
+                        }}><i className="fa-solid fa-chevron-right"></i></button>
+                      </p>
+                      <div className="price">
+                        <p><strong>Price per item:</strong> ${value.product.price}</p>
+                        <p><strong>Total price:</strong> ${money.mul(value.quantity.toFixed(2).toString(), value.product.price.toFixed(2).toString())}</p>
                       </div>
-                    </li>
-                  </ul>
-                ))}
-              </div>
-              <div className="price-cart">
-                <div className="detail-price">
-                  <p>Total price:</p>
-                  <p className="price-detail">${totalPrice}</p>
-                  <p>Taxes:</p>
-                  <p className="price-detail">$0</p>
-                  <p>Service use:</p>
-                  <p className="price-detail">$0</p>
-                  <p>Total</p>
-                  <p className="price-detail">${totalPrice}</p>
-                </div>
-                <button>Comprar</button>
-                <hr />
-              </div>
+                    </div>
+                  </div>
+                </li>
+              ))}
+            </ul>
+          </div>
+          <div className="price-cart">
+            <div className="detail-price">
+              <p>Total price:</p>
+              <p className="price-detail">${totalPrice}</p>
+              <p>Taxes:</p>
+              <p className="price-detail">$0</p>
+              <p>Service use:</p>
+              <p className="price-detail">$0</p>
+              <p>Total</p>
+              <p className="price-detail">${totalPrice}</p>
+            </div>
+            <button>Comprar</button>
+            <hr />
+          </div>
             
 
 
